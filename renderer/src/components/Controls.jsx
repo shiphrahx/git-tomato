4601201a028325dd1df2613f27cfa3323040e823
Shiphrah <lisa.shiphrah@gmail.com>
2026-03-23 08:18:41 +0000

    Fix broken restart and settings SVG icons in timer controls

--- renderer/src/components/Controls.jsx
+++ renderer/src/components/Controls.jsx
@@ -53,17 +53,17 @@ export function Controls({ status, type, onStart, onPause, onReset, onSelectFocu
 
         {/* Restart */}
         <button className="ctrl-btn ctrl-btn--secondary" onClick={onReset} title="Restart">
-          <svg width="24" height="24" viewBox="0 0 20 20" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-            <path d="M3.5 6A7.5 7.5 0 1 1 3 10.5" />
-            <polyline points="1,3 3.5,6 6,3" />
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M3 12a9 9 0 1 0 9-9 9 9 0 0 0-6.36 2.64L3 8" />
+            <polyline points="3,3 3,8 8,8" />
           </svg>
         </button>
 
         {/* Settings */}
         <button className="ctrl-btn ctrl-btn--ghost" onClick={onConfig} title="Settings">
-          <svg width="24" height="24" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" clipRule="evenodd" d="M10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm0 1.5a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9z" />
-            <path fillRule="evenodd" clipRule="evenodd" d="M8.47 1.07a1 1 0 0 1 3.06 0l.35 1.22a7.05 7.05 0 0 1 1.77 1.02l1.24-.3a1 1 0 0 1 1.1.5l.53.92a1 1 0 0 1-.18 1.2l-.91.81a7.1 7.1 0 0 1 0 2.04l.91.81a1 1 0 0 1 .18 1.2l-.53.92a1 1 0 0 1-1.1.5l-1.24-.3a7.05 7.05 0 0 1-1.77 1.02l-.35 1.22a1 1 0 0 1-3.06 0l-.35-1.22A7.05 7.05 0 0 1 6.35 12l-1.24.3a1 1 0 0 1-1.1-.5l-.53-.92a1 1 0 0 1 .18-1.2l.91-.81a7.1 7.1 0 0 1 0-2.04l-.91-.81a1 1 0 0 1-.18-1.2l.53-.92a1 1 0 0 1 1.1-.5l1.24.3A7.05 7.05 0 0 1 8.12 2.3l.35-1.22z" />
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <circle cx="12" cy="12" r="3" />
+            <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1-2.83 2.83l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-4 0v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83-2.83l.06-.06A1.65 1.65 0 0 0 4.68 15a1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1 0-4h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 2.83-2.83l.06.06A1.65 1.65 0 0 0 9 4.68a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 4 0v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 2.83l-.06.06A1.65 1.65 0 0 0 19.4 9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 0 4h-.09a1.65 1.65 0 0 0-1.51 1z" />
           </svg>
         </button>
       </div>
